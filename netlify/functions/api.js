6eb0b370d15451adf33072b118b8bac63b211f52
cnbkrtlverve <canbakirtel@uniqstore.com.tr>
2025-08-24 21:13:09 +0300

    v1.2.2

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,5 +1,5 @@
 import serverless from 'serverless-http';
-import app from './dist/server'; // Derlenmiş sunucu dosyasını import et
+import app from '../../dist/server.js'; // Derlenmiş sunucu dosyasını doğru yoldan import et
 
 // Express app'ini serverless bir fonksiyona çevir
 export const handler = serverless(app);
